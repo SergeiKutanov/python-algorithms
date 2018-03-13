8acd3ea658226c226f1fd67f0758e1840e93564a
sergeikutanov <sergeikutanov@gmail.com>
2018-03-13 13:15:30 -0700

    randomized quicksort

---
+++ sorting/randomized_quicksort.py
@@ -0,0 +1,40 @@
+import random
+
+
+def randomized_partition(data, start, end):
+
+    random_pivot = random.randint(start, end + 1)
+    data[random_pivot], data[end] = data[end], data[random_pivot]
+
+    x = data[end]
+    i = start - 1
+    for j in range(start, end):
+        if data[j] <= x:
+            i = i + 1
+            data[i], data[j] = data[j], data[i]
+    data[i + 1], data[end] = data[end], data[i + 1]
+    return i + 1
+
+
+def quicksort(data, start, end):
+    if start < end:
+        q = randomized_partition(data, start, end)
+        quicksort(data, start, q - 1)
+        quicksort(data, q + 1, end)
+
+
+def get_data():
+    return [
+        2, 8, 7, 1, 3, 5, 6, 4
+    ]
+
+
+def main():
+    data = get_data()
+    print(data)
+    quicksort(data, 0, len(data) - 1)
+    print(data)
+
+
+if __name__ == "__main__":
+    main()
